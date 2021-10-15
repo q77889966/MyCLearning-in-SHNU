39671e591217110ffbbe96744ea2ff4b40bcd079
q77889966 <2304144391@qq.com>
2021-10-15 21:18:20 +0800

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main() {
+	int eg, num, oth;
+	eg = 0;
+	num = 0;
+	oth = 0;
+
+	char x;
+	while ((x = getchar()) != '\n'){
+		if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z')) 
+			eg++;
+
+		else if (x >= '1' && x <= '9') {
+			num++;
+		}
+
+		else oth++;
+	}
+
+	printf("%d %d %d", eg, num, oth);
+}
