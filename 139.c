20c8df2cfdbb2f016b2db84a992e0b3f43d1c543
Alan_Chen <2304144391@qq.com>
2021-11-14 10:33:09 +0800

    139

---
+++ 139.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int i, j;
+	int arr[3][3];
+	int sum = 0;
+
+	for (i = 0; j <= 2; i++) {
+		for (j = 0; j <= 2; j++) {
+			scanf_s("%d", &arr[i][j][k]);
+		}
+	}
+	for (i = 0; i <= 2; i++) {
+		sum=sum+arr[1][i]*arr
+	}
+	return 0;
+}
